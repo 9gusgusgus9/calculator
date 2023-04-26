0fe56b3f706bb10cd7651cd49970aa3e86da55e9
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2023-04-26 16:10:16 +0200

    Solved error for C and CE buttons, also implemented the possibility to use ans of previous operation

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let input = document.getElementById('input');
 
 class Calculator {
-    input1; input2; operand; result;
+    input1; input2; operand; result; lastRes;
 
     constructor() {
-        this.input1 = '';
-        this.input2 = '';
-        this.operator = '';
-        this.result = '0';
+        this.clear()
     }
 
     insert(char) {
         if(this.operator === '') {
-            if (this.input1 === result){
+            if (this.input1 === this.result){
                 input.value = '';
             }
             this.input1 += char;
@@ -24,29 +21,51 @@ class Calculator {
     }
 
     operate(operator) {
+        if (this.input1 !== ''){
+            this.input1 = input.value;
+        } else {
+            this.input1 = this.result;
+        }
         this.operator = operator;
-        this.input1 = input.value;
     }
 
     calculate() {
         let result = 0;
         switch (this.operator) {
-            case "+":
-                result = Number(this.input1) + Number(this.input2);
+            case "%":
+                if (this.input2 === '') {
+                    result = Number(this.input1) / 100;
+                } else {
+                    result = 'Error';
+                }
                 break;
-            case "-":
-                result = Number(this.input1) - Number(this.input2);
-                break;
-            case "*":
-                result = Number(this.input1) * Number(this.input2);
-                break;
-            case "/":
-                result = Number(this.input1) / Number(this.input2);
-                break;
+            default :
+                if(this.input1 !== '' && this.input2 !== ''){
+                    switch (this.operator) {
+                        case "+":
+                            result = Number(this.input1) + Number(this.input2);
+                            break;
+                        case "-":
+                            result = Number(this.input1) - Number(this.input2);
+                            break;
+                        case "*":
+                            result = Number(this.input1) * Number(this.input2);
+                            break;
+                        case "/":
+                            result = Number(this.input1) / Number(this.input2);
+                        break;
+                    }
+                }
         }
-        this.input1 = result;
-        this.input2 = '';
-        this.operator = '';
+        if (result !== 'Error') {
+            this.input1 = result;
+            this.input2 = '';
+            this.operator = '';
+            this.lastRes = true;
+        } else {
+            this.clear();
+            this.lastRes = false;
+        }
         input.value = result;
     }
 
@@ -55,13 +74,17 @@ class Calculator {
         this.input2 = '';
         this.operator = '';
         this.result = '0';
+        this.lastRes = false;
+        input.value = this.result;
     }
 
     backspace() {
         if (this.operator === '') {
             this.input1 = this.input1.slice(0, -1);
+            input.value = this.input1;
         } else {
             this.input2 = this.input2.slice(0, -1);
+            input.value = this.input2;
         }
     }
 
